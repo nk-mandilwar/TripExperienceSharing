2d349634512db69d5138b10db7ac6cffa9f63cdd
nk-mandilwar <nitinkumarmandilwar5@gmail.com>
2019-05-18 16:39:23 +0530

    Set app layout and configured devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+	
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  # to set the path after user signs in
+	def after_sign_in_path_for(resource)
+  	root_path
+	end
+
+  # to set the path after user logs out
+	def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
